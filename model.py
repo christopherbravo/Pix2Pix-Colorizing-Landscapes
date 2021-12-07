5487ba3465596dec210c86c90378a3dcfe098781
Rohan Gupta <rohangupta2036@gmail.com>
2021-12-07 18:33:25 -0500

    running?

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from preprocess import get_data
 import tensorflow as tf
+from matplotlib import pyplot as plt
 
 class Generator(tf.keras.Model):
     pass
@@ -74,8 +75,7 @@ class Model(tf.keras.Model):
         self.discriminator.add(create_layer_with_batch_norm_and_relu(1,4,batch_norm=False))
         self.discriminator.add(tf.keras.layers.Activation('sigmoid'))
 
-    def call(self,original_images,real_transformed_images):
-        # call the generator
+    def generate(self,original_images):
         encoder = self.generator[0]
         decoder = self.generator[1]
         final_conv = self.generator[2]
@@ -96,6 +96,14 @@ class Model(tf.keras.Model):
 
         curr_output = final_conv(curr_output)
         curr_output = final_tanh(curr_output)
+        return curr_output
+
+    def discriminate(self,original_images,real_transformed_images):
+        pass
+
+    def call(self,original_images,real_transformed_images):
+        # call the generator
+        curr_output = self.generate(original_images)
 
         original_and_real_transformed = tf.keras.layers.Concatenate()([original_images[0],real_transformed_images[0]])
         logits_real_given_real = self.discriminator(original_and_real_transformed) # check what concatenating them like this does
@@ -154,7 +162,25 @@ def main():
     real_transformed_images = list(real_transformed_images)
 
     model = Model(3)
-    train(model,original_images,real_transformed_images)
+    train(model,original_images[:10],real_transformed_images[:10])
+
+    def generate_images(model, test_input, tar):
+      prediction = model.generate(test_input)
+      plt.figure(figsize=(15, 15))
+
+      display_list = [test_input[0], tar[0], prediction[0]]
+      title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+      for i in range(3):
+        plt.subplot(1, 3, i+1)
+        plt.title(title[i])
+        # Getting the pixel values in the [0, 1] range to plot.
+        plt.imshow(display_list[i] * 0.5 + 0.5)
+        plt.axis('off')
+      plt.show()
+
+    for example_input, example_target in test_data.take(1):
+        generate_images(model, example_input, example_target)
 
 
 if __name__ == '__main__':
